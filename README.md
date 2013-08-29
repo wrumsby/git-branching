193f1ee69a1fa266f60547ac4b7da830be1fb354
Walter Rumsby <walter@rumsby.net>
2013-08-30 08:04:43 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Git Branching
+
+Internal Presentation
+
+[View Slides](http://zenp.azurewebsites.net/index.html?slides=https://raw.github.com/wrumsby/git-branching/master/) (this will probably only work in Chrome ATM) 
---
+++ slides.js
@@ -0,0 +1,36 @@
+var SLIDES = [
+	{
+		"title": "Branching is cheap",
+		"image": "http://www.flickr.com/photos/bruce_mcadam/311276113/",
+		"cls": "darker"
+	},
+
+	{
+		"quote": {
+			"text": "The best thing that can be said of [any piece of] software is that it is too small",
+			"from": "James Halliday aka @substack"
+		},
+		"image": "http://www.flickr.com/photos/theodorescott/8607310734/",
+		"notes": "unix philosophy - @substack, most modules on npm",
+		"cls": "top-30"
+	},
+
+	{
+		"image": "http://www.flickr.com/photos/wrumsby/9625198096/"
+	},
+
+	{
+		"image": "http://www.flickr.com/photos/falequin/8443342362/",
+		"notes": "modular thinking; tiny modules - really simple modules that only do 1 thing"
+	},
+
+	{
+		"gist": "https://gist.github.com/substack/5075355",
+		"notes": "npm size - number of modules; node philosophy; package manager war"
+	},
+
+	{
+		"image": "http://www.flickr.com/photos/insouciance/3061759623/",
+		"title": "sweet workflow"
+	}
+];
